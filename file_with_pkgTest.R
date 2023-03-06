c9951440e4ea04b52281d681411b8ca525ba2974
suraiya14 <58765445+suraiya14@users.noreply.github.com>
2023-03-06 12:13:44 -0800

    Delete file_with_pkgTest.R

--- file_with_pkgTest.R
+++
@@ -1,8 +0,0 @@
-pkgTest <- function(x)
-{
-  if (!require(x,character.only = TRUE))
-  {
-    install.packages(x,dep=TRUE,repos="http://cran.us.r-project.org")
-    if(!require(x,character.only = TRUE)) stop("Package not found")
-  }
-}
