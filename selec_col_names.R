35f7f02e7c36132fbf0b563ce9bb8ded572bfb56
suraiya14 <58765445+suraiya14@users.noreply.github.com>
2023-03-06 12:07:16 -0800

    Delete selec_col_names.R

--- selec_col_names.R
+++
@@ -1,7 +0,0 @@
-df<-read.csv("D:\\PARGT_Windows\\new_with_8CTD\\new_with_8CTD\\p85\\selected_train_test_merged_file.csv")
-nc<-ncol(df)
-df<-df[,-nc]
-clnm<-colnames(df)
-
-
-paste0("'",paste0(clnm, sep="", collapse="','"),"'")
